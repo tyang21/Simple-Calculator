5f26d671e2290a7c5b3e5ad89f0753ca6156de47
tyang21 <42978521+tyang21@users.noreply.github.com>
2018-09-17 14:30:17 -0400

    Add files via upload

---
+++ src/CalculatorMain.java
@@ -0,0 +1,12 @@
+
+public class CalculatorMain {
+
+	public static void main(String[] args) 
+	{ 
+		Calculator calc = new Calculator();
+		System.out.print(calc add(2,4));
+		// TODO Auto-generated method stub
+
+	}
+
+}
